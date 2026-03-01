e4ae2d48e572a577ad25491acc88ac5e5406b2b2
Rahma Ayari <rahmaayari272@gmail.com>
2026-03-01 13:17:36 +0100

    feat: implémentation de la liste des suggestions avec like, favoris et recherche
    - Ajout des boutons "Like" et "Ajouter aux favoris"
      - Les boutons ne s’affichent pas si l’état de la suggestion est "refusée"
      - Le bouton "Like" incrémente le nombre de likes
      - Le bouton "Ajouter aux favoris" ajoute la suggestion à la liste des favoris
    - Ajout d’une barre de recherche filtrant les suggestions par titre et catégorie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ListSuggestionComponent
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
+    CommonModule,
+    FormsModule 
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
